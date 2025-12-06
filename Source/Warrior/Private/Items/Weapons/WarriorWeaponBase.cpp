1bba6d8198ec468d5638a465db95e8d52809981b
Keyblader333 <Keyblader333@gmail.com>
2025-12-05 23:43:02 -0600

    SetUp Weapon Base and Hero Weapon classes, Created Hero Weapon Base BP and Axe Subclass, Adjusted Collision box settings

---
+++ Source/Warrior/Private/Items/Weapons/WarriorWeaponBase.cpp
@@ -0,0 +1,20 @@
+// Daniel McPherson All Rights Reserved
+
+
+#include "Items/Weapons/WarriorWeaponBase.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AWarriorWeaponBase::AWarriorWeaponBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>("WeaponMesh");
+	SetRootComponent(WeaponMesh);
+
+	WeaponCollisionBox = CreateDefaultSubobject<UBoxComponent>("WeaponCollisionBox");
+	WeaponCollisionBox->SetupAttachment(GetRootComponent());
+	WeaponCollisionBox->SetBoxExtent(FVector(20.f));
+	WeaponCollisionBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
