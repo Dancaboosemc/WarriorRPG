eae4ab2aa716a4992f7c40b35b17da9f688d66fb
Keyblader333 <Keyblader333@gmail.com>
2025-12-16 12:27:59 -0600

    Implemented WarriorAttributeSet

--- Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
+++ Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
@@ -3,3 +3,12 @@
 
 #include "AbilitySystem/WarriorAttributeSet.h"
 
+UWarriorAttributeSet::UWarriorAttributeSet()
+{
+	InitMaxHealth(1.f);
+	InitCurrentHealth(GetMaxHealth());
+	InitMaxRage(1.f);
+	InitCurrentRage(GetMaxRage());
+	InitAttackPower(1.f);
+	InitDefensePower(1.f);
+}
