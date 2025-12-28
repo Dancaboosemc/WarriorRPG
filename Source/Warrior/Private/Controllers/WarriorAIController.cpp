ec4b62f902f7cfd77a21cd857708f4600eb3bfa1
Keyblader333 <Keyblader333@gmail.com>
2025-12-28 12:15:08 -0600

    Changed Implementation for WarriorAIController::GetAttitudeTowards() to consider teams of lower priority as hositle so AI only targets the hero character, Changed Implementation of WarriorAIController::OnEnemyPerceptionUpdated() to only run if not TargetActor is set, Created and Implemented EQS_FindStraifingLocation, Created EQSContext_TargetActor to supply context to EQS_FindStrafingLocation, Implemented move to straf location in BT_Guardian.

--- Source/Warrior/Private/Controllers/WarriorAIController.cpp
+++ Source/Warrior/Private/Controllers/WarriorAIController.cpp
@@ -33,7 +33,7 @@ ETeamAttitude::Type AWarriorAIController::GetTeamAttitudeTowards(const AActor& O
 	const APawn* PawnToCheck = Cast<const APawn>(&Other);
 	const IGenericTeamAgentInterface* OtherTeamAgent = Cast<const IGenericTeamAgentInterface>(PawnToCheck->GetController());
 
-	if (OtherTeamAgent && OtherTeamAgent->GetGenericTeamId() != GetGenericTeamId())
+	if (OtherTeamAgent && OtherTeamAgent->GetGenericTeamId() < GetGenericTeamId())
 	{
 		return ETeamAttitude::Hostile;
 	}
@@ -75,11 +75,15 @@ void AWarriorAIController::BeginPlay()
 
 void AWarriorAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
-	if (Stimulus.WasSuccessfullySensed() && Actor)
+
+	if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
 	{
-		if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
+		if (!BlackboardComponent->GetValueAsObject(FName("TargetActor")))
 		{
-			BlackboardComponent->SetValueAsObject(FName("TargetActor"), Actor);
+			if (Stimulus.WasSuccessfullySensed() && Actor)
+			{
+				BlackboardComponent->SetValueAsObject(FName("TargetActor"), Actor);
+			}
 		}
 	}
 }
