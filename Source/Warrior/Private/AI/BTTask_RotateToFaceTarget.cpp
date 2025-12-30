384ff350d2cfb8e0c9236b61dc4a141fb6c550ee
Keyblader333 <Keyblader333@gmail.com>
2025-12-29 21:33:39 -0600

    Created Custom BTTask from BTNode for rotating actor to face target and Implemented basic functionaility to be usable in behavior tree asset

---
+++ Source/Warrior/Private/AI/BTTask_RotateToFaceTarget.cpp
@@ -0,0 +1,41 @@
+// Daniel McPherson All Rights Reserved
+
+
+#include "AI/BTTask_RotateToFaceTarget.h"
+
+UBTTask_RotateToFaceTarget::UBTTask_RotateToFaceTarget()
+{
+	NodeName = TEXT("Native Rotate toFace Target Actor");
+	AnglePrecision = 10.f;
+	RotationInterpSpeed = 5.f;
+
+	bNotifyTick = true;
+	bNotifyTaskFinished = true;
+	bCreateNodeInstance = false;
+
+	INIT_TASK_NODE_NOTIFY_FLAGS();
+
+	InTargetToFaceKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(ThisClass, InTargetToFaceKey), AActor::StaticClass());
+}
+
+void UBTTask_RotateToFaceTarget::InitializeFromAsset(UBehaviorTree& Asset)
+{
+	Super::InitializeFromAsset(Asset);
+
+	if (UBlackboardData* BBA = GetBlackboardAsset())
+	{
+		InTargetToFaceKey.ResolveSelectedKey(*BBA);
+	}
+}
+
+uint16 UBTTask_RotateToFaceTarget::GetInstanceMemorySize() const
+{
+	return sizeof(FRotateToFaceTargetTaskMemory);
+}
+
+FString UBTTask_RotateToFaceTarget::GetStaticDescription() const
+{
+	const FString KeyDescription = InTargetToFaceKey.SelectedKeyName.ToString();
+
+	return FString::Printf(TEXT("Smoothly rotates to face %s Key until the angle precision %s is reached"), *KeyDescription, *FString::SanitizeFloat(AnglePrecision));	
+}
